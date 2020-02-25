7c206fc333392cd9d5eee64948f6772d9e123eb7
John Doe <30752740@qq.com>
2020-02-25 08:22:10 +0800

    更新

--- spark-streaming/src/main/scala/com/atguigu/spark/steaming/project/app/LastHourAdsClickApp.scala
+++ spark-streaming/src/main/scala/com/atguigu/spark/steaming/project/app/LastHourAdsClickApp.scala
@@ -1,4 +1,5 @@
 package com.atguigu.spark.steaming.project.app
+
 import com.atguigu.spark.steaming.project.bean.AdsInfo
 import org.apache.spark.streaming.dstream.DStream
 
@@ -6,8 +7,9 @@ import org.apache.spark.streaming.dstream.DStream
   * Author atguigu
   * Date 2020/2/24 14:37
   */
-object LastHourAdsClickApp extends App{
+object LastHourAdsClickApp extends App {
     override def doSomething(adsInfoStream: DStream[AdsInfo]): Unit = {
+    
     
     }
 }
